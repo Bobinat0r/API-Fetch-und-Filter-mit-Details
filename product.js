d867901c61ec7ccf1fb38292e61a93e71ff67c81
Bobinat0r <francesco.bellina007@gmail.com>
2025-02-26 11:00:55 +0100

    Füge Produktanzeige in Tabelle hinzu und implementiere Fetch-API für Produktdaten

--- product.js
+++ product.js
@@ -1,7 +1,36 @@
-
 // Could be GET or POST/PUT/PATCH/DELETE
-fetch('https://dummyjson.com/test')
-.then(res => res.json())
-.then(console.log);
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    const products = data.products.map((product) => ({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        images: product.images,
+        thumbnail: product.thumbnail,
+        description: product.description,
+    }));
+  
+    const table = document.getElementById("productTable");
+    products.forEach((element) => {
+      const row = table.insertRow();
+      row.insertCell().textContent = element.id;
+      row.insertCell().textContent = element.name;
+      row.insertCell().textContent = element.price;
+      const img = document.createElement("img");
+      img.src = element.thumbnail;
+      img.width = 100;
+      img.height = 100;
+      row.insertCell().appendChild(img);
+      const button = document.createElement("button");
+      row.insertCell().appendChild(button);
+        button.textContent = "View";
+        button.onclick = () => showProductDetails(element)
 
-/* { status: 'ok', method: 'GET' } */
+    });
+});  
+
+function showProductDetails (product) {
+    console.log(product);
+    
+}
